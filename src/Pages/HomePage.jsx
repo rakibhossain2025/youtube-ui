914408e5c6821003e0a94da6f8e516f444f3fc60
rakibhossain2025 <rakibhossain4230@gmail.com>
2025-07-25 20:51:26 +0600

    ☺️

---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import NavBar from '../Components/Navber/Navbar';
+
+const HomePage = () => {
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+
+      </main>
+    </>
+  );
+};
+
+export default HomePage;
